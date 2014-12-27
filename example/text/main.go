97440eee3deb30a2c4f10213fb49c974231e6d5f
Stan Steel <steel@kryas.com>
2014-12-27 14:43:27 -0700

    performance improvement

--- example/text/main.go
+++ example/text/main.go
@@ -14,9 +14,120 @@ func main() {
 	fill.SetMargin(ui.Margin{3, 3, 3, 3})
 	txt := text.New(fill, "",
 		`
-		ajsdkfjasdlkjfhaskdljfhasdlf
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
+Generally, open source refers to a computer program in which the source code is available to the general public for use and/or modification from its original design. Open-source code is typically a collaborative effort where programmers improve upon the source code and share the changes within the community so that other members can help improve it further.
+
+Many large formal institutions have sprung up to support the development of the open source movement, including the Apache Software Foundation, which supports projects such as the open source framework behind big data Apache Hadoop and an open-source HTTP server Apache HTTP.
+
+The open-source model is based on a more decentralized model of production, in contrast with more centralized models of development such as those typically used in commercial software companies.[4]
+
+A main principle of open-source software development is peer production by collaboration, with the end-product, source code, "blueprints", and documentation available at no cost to the public. The open source movement in software began as a response to the limitations of closed proprietary code, and it is now spreading across different fields. This model is also used for the development of open-source-appropriate technologies,[5] solar photovoltaic technology[6] and open-source drug discovery.[7][8]
+
 		`)
-	txt.Foreground = color.Blue3
+	txt.Foreground = color.Gray13
 	txt.AddMousePositionCB(func(x, y float64) {
 		fmt.Println(x, y)
 	})
--- text/text.go
+++ text/text.go
@@ -1,21 +1,30 @@
 package text
 
 import (
+	"fmt"
 	"github.com/vizstra/ui"
 	. "github.com/vizstra/ui/color"
 	"github.com/vizstra/vg"
+	"strings"
 )
 
 type Text struct {
 	ui.Element
-	text string
+	text     string
+	tokens   []string
+	WrapText bool
 }
 
 func New(parent ui.Drawer, name, text string) *Text {
 	self := &Text{
 		ui.NewElement(parent, name),
 		text,
+		strings.Fields(text),
+		true,
 	}
+	self.Background = White
+	self.Foreground = Blue5
+
 	return self
 }
 
@@ -29,10 +38,35 @@ func (self *Text) Draw(x, y, w, h float64, ctx vg.Context) {
 
 	ctx.SetFontPtSize(23)
 	_, _, lineh := ctx.TextMetrics()
-	_, ly := x, y+lineh
+	tx, ty := x, y+lineh
 	ctx.FillColor(self.Foreground)
 
-	ctx.WrappedText(x, ly, w, self.text)
+	xmin, _, xmax, _ := ctx.TextBounds("o", 0, 0)
+	spaceWidth := (xmax - xmin)
+	farEdge := x + w
+	for i, s := range self.tokens {
+		xmin, _, xmax, _ := ctx.TextBounds(s, tx, ty)
+		if i == -1 {
+			fmt.Println(xmax, xmin, x)
+		}
+
+		if xmax > farEdge {
+			if self.WrapText {
+				tx = x
+				ty += lineh
+			} else if xmin > farEdge {
+				break
+			}
+		}
+
+		if ty > y+h+lineh {
+			break
+		}
+
+		ctx.Text(tx, ty, s)
+		tx += (xmax - xmin) + spaceWidth
+
+	}
 	ctx.ResetScissor()
 }
 
